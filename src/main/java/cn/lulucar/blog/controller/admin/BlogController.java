7a1b0b29c2336a9c4d33438166c0f442418d674e
wenxiaolan <wenxiaolan02@outlook.com>
2024-05-01 00:43:29 +0800

    新增 /blog/list 和 /blogs 页面访问

--- src/main/java/cn/lulucar/blog/controller/admin/BlogController.java
+++ src/main/java/cn/lulucar/blog/controller/admin/BlogController.java
@@ -1,10 +1,18 @@
 package cn.lulucar.blog.controller.admin;
 
+import cn.hutool.core.util.ObjectUtil;
 import cn.lulucar.blog.service.BlogService;
 import cn.lulucar.blog.service.CategoryService;
+import cn.lulucar.blog.util.PageQueryUtil;
+import cn.lulucar.blog.util.Result;
+import cn.lulucar.blog.util.ResultGenerator;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @author wenxiaolan
@@ -18,5 +26,36 @@ public class BlogController {
     private BlogService blogService;
     @Resource
     private CategoryService categoryService;
+    
+    // todo 完成 Blog 的功能
+
+    /**
+     * 博客列表数据
+     * @param params 分页参数
+     * @return 返回分页后的博客列表数据
+     */
+    @GetMapping("/blogs/list")
+    @ResponseBody
+    public Result list(@RequestParam Map<String, Object> params) {
+        if (ObjectUtils.isEmpty(params.get("page")) || ObjectUtils.isEmpty(params.get("limit"))) {
+            return ResultGenerator.genFailResult("参数异常");
+        }
+        PageQueryUtil pageQueryUtil = new PageQueryUtil(params);
+        return ResultGenerator.genSuccessResult(blogService.getBlogsPage(pageQueryUtil));
+    }
+
+    /**
+     * 博客管理模块
+     * @param request 请求
+     * @return 跳转到博客管理页面
+     */
+    @GetMapping("/blogs")
+    public String list(HttpServletRequest request) {
+        request.setAttribute("path","blogs");
+        return "admin/blog";
+    }
+    
+    
+    
     
 }
