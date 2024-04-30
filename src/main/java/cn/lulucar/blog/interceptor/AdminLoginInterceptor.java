54269d42454d84133171313cefc78f9e17fb612d
wenxiaolan <wenxiaolan02@outlook.com>
2024-05-01 00:37:23 +0800

    修改 AdminLoginInterceptor 日志输出

--- src/main/java/cn/lulucar/blog/interceptor/AdminLoginInterceptor.java
+++ src/main/java/cn/lulucar/blog/interceptor/AdminLoginInterceptor.java
@@ -19,14 +19,14 @@ public class AdminLoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
         String requestServletPath = request.getServletPath();
-        log.info(requestServletPath);
+        log.info("请求路径：{}",requestServletPath);
         if (requestServletPath.startsWith("/admin") && null == request.getSession().getAttribute("loginUser")) {
             request.getSession().setAttribute("errorMsg", "请重新登陆");
             response.sendRedirect(request.getContextPath() + "/admin/login");
             return false;
         } else {
             request.getSession().removeAttribute("errorMsg");
-            return true;
+            return true;    
         }
     }
 
