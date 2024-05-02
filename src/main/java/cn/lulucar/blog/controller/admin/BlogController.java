22173a795be2558e8a7ddd48be00ac8e9a043258
wenxiaolan <wenxiaolan02@outlook.com>
2024-05-02 23:15:23 +0800

    新增 发布、保存和更新博客的功能

--- src/main/java/cn/lulucar/blog/controller/admin/BlogController.java
+++ src/main/java/cn/lulucar/blog/controller/admin/BlogController.java
@@ -1,6 +1,7 @@
 package cn.lulucar.blog.controller.admin;
 
 import cn.hutool.core.util.ObjectUtil;
+import cn.lulucar.blog.entity.Blog;
 import cn.lulucar.blog.service.BlogService;
 import cn.lulucar.blog.service.CategoryService;
 import cn.lulucar.blog.util.PageQueryUtil;
@@ -8,8 +9,10 @@ import cn.lulucar.blog.util.Result;
 import cn.lulucar.blog.util.ResultGenerator;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -19,6 +22,7 @@ import java.util.Map;
  * @ClassName BlogController
  * @date 2024/4/29 22:49
  */
+@Slf4j
 @Controller
 @RequestMapping("/admin")
 public class BlogController {
@@ -45,7 +49,7 @@ public class BlogController {
     }
 
     /**
-     * 博客管理模块
+     * 博客管理页面
      * @param request 请求
      * @return 跳转到博客管理页面
      */
@@ -54,8 +58,169 @@ public class BlogController {
         request.setAttribute("path","blogs");
         return "admin/blog";
     }
-    
-    
+
+    /**
+     * 发布/修改博客
+     * @param request 请求
+     * @return 跳转到发布博客页面
+     */
+    @GetMapping("/blogs/edit")
+    public String edit(HttpServletRequest request) {
+        request.setAttribute("path", "edit");
+        request.setAttribute("categories", categoryService.getAllCategories());
+        return "admin/edit";
+    }
+
+    /**
+     * 根据 blogId 编辑博客
+     * @param request 请求
+     * @param blogId 博客Id
+     * @return
+     */
+    @GetMapping("/blogs/edit/{blogId}")
+    public String edit(HttpServletRequest request, @PathVariable("blogId") Long blogId) {
+        request.setAttribute("path", "edit");
+        Blog blog = blogService.getBlogById(blogId);
+        if (blog == null) {
+            log.error("未找到该博客");
+            return "error/error_400";
+        }
+        log.info("blogId={}",blogId);
+        request.setAttribute("blog", blog);
+        request.setAttribute("categories", categoryService.getAllCategories());
+        return "admin/edit";
+    }
+
+    /**
+     * 保存博客
+     * 
+     * @param blogTitle 博客标题
+     * @param blogSubUrl 博客子Url
+     * @param blogCategoryId 博客分类Id
+     * @param blogTags 博客标签
+     * @param blogContent 博客内容
+     * @param blogCoverImage 博客封面
+     * @param blogStatus 博客发布状态
+     * @param enableComment 能否评论
+     * @return
+     */
+    @PostMapping("/blogs/save")
+    @ResponseBody
+    public Result save(@RequestParam("blogTitle") String blogTitle,
+                       @RequestParam(name = "blogSubUrl", required = false) String blogSubUrl,
+                       @RequestParam("blogCategoryId") Integer blogCategoryId,
+                       @RequestParam("blogTags") String blogTags,
+                       @RequestParam("blogContent") String blogContent,
+                       @RequestParam("blogCoverImage") String blogCoverImage,
+                       @RequestParam("blogStatus") Byte blogStatus,
+                       @RequestParam("enableComment") Byte enableComment) {
+        if (!StringUtils.hasText(blogTitle)) {
+            return ResultGenerator.genFailResult("请输入文章标题");
+        }
+        if (blogTitle.trim().length() > 150) {
+            return ResultGenerator.genFailResult("标题过长");
+        }
+        if (!StringUtils.hasText(blogTags)) {
+            return ResultGenerator.genFailResult("请输入文章标签");
+        }
+        if (blogTags.trim().length() > 150) {
+            return ResultGenerator.genFailResult("标签过长");
+        }
+        if (blogSubUrl.trim().length() > 150) {
+            return ResultGenerator.genFailResult("路径过长");
+        }
+        if (!StringUtils.hasText(blogContent)) {
+            return ResultGenerator.genFailResult("请输入文章内容");
+        }
+        if (blogTags.trim().length() > 100000) {
+            return ResultGenerator.genFailResult("文章内容过长");
+        }
+        if (!StringUtils.hasText(blogCoverImage)) {
+            return ResultGenerator.genFailResult("封面图不能为空");
+        }
+        Blog blog = new Blog();
+        blog.setBlogTitle(blogTitle);
+        blog.setBlogSubUrl(blogSubUrl);
+        blog.setBlogCategoryId(blogCategoryId);
+        blog.setBlogTags(blogTags);
+        blog.setBlogContent(blogContent);
+        blog.setBlogCoverImage(blogCoverImage);
+        blog.setBlogStatus(blogStatus);
+        blog.setEnableComment(enableComment);
+        String saveBlogResult = blogService.saveBlog(blog);
+        if ("success".equals(saveBlogResult)) {
+            return ResultGenerator.genSuccessResult("添加成功");
+        } else {
+            return ResultGenerator.genFailResult(saveBlogResult);
+        }
+    }
+
+    /**
+     * 更新博客
+     * 
+     * @param blogId
+     * @param blogTitle
+     * @param blogSubUrl
+     * @param blogCategoryId
+     * @param blogTags
+     * @param blogContent
+     * @param blogCoverImage
+     * @param blogStatus
+     * @param enableComment
+     * @return
+     */
+    @PostMapping("/blogs/update")
+    @ResponseBody
+    public Result update(@RequestParam("blogId") Long blogId,
+                         @RequestParam("blogTitle") String blogTitle,
+                         @RequestParam(name = "blogSubUrl", required = false) String blogSubUrl,
+                         @RequestParam("blogCategoryId") Integer blogCategoryId,
+                         @RequestParam("blogTags") String blogTags,
+                         @RequestParam("blogContent") String blogContent,
+                         @RequestParam("blogCoverImage") String blogCoverImage,
+                         @RequestParam("blogStatus") Byte blogStatus,
+                         @RequestParam("enableComment") Byte enableComment) {
+        if (!StringUtils.hasText(blogTitle)) {
+            return ResultGenerator.genFailResult("请输入文章标题");
+        }
+        if (blogTitle.trim().length() > 150) {
+            return ResultGenerator.genFailResult("标题过长");
+        }
+        if (!StringUtils.hasText(blogTags)) {
+            return ResultGenerator.genFailResult("请输入文章标签");
+        }
+        if (blogTags.trim().length() > 150) {
+            return ResultGenerator.genFailResult("标签过长");
+        }
+        if (blogSubUrl.trim().length() > 150) {
+            return ResultGenerator.genFailResult("路径过长");
+        }
+        if (!StringUtils.hasText(blogContent)) {
+            return ResultGenerator.genFailResult("请输入文章内容");
+        }
+        if (blogTags.trim().length() > 100000) {
+            return ResultGenerator.genFailResult("文章内容过长");
+        }
+        if (!StringUtils.hasText(blogCoverImage)) {
+            return ResultGenerator.genFailResult("封面图不能为空");
+        }
+        Blog blog = new Blog();
+        blog.setBlogId(blogId);
+        blog.setBlogTitle(blogTitle);
+        blog.setBlogSubUrl(blogSubUrl);
+        blog.setBlogCategoryId(blogCategoryId);
+        blog.setBlogTags(blogTags);
+        blog.setBlogContent(blogContent);
+        blog.setBlogCoverImage(blogCoverImage);
+        blog.setBlogStatus(blogStatus);
+        blog.setEnableComment(enableComment);
+        String updateBlogResult = blogService.updateBlog(blog);
+        if ("success".equals(updateBlogResult)) {
+            return ResultGenerator.genSuccessResult("修改成功");
+        } else {
+            return ResultGenerator.genFailResult(updateBlogResult);
+        }
+    }
     
     
 }
