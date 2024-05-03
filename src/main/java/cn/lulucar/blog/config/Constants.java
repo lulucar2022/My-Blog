936a695af8e582e41e64437fba3968ecae19cd6c
wenxiaolan <wenxiaolan02@outlook.com>
2024-05-03 16:14:27 +0800

    修改 文件上传位置

--- src/main/java/cn/lulucar/blog/config/Constants.java
+++ src/main/java/cn/lulucar/blog/config/Constants.java
@@ -1,8 +1,11 @@
 package cn.lulucar.blog.config;
 
-/**
- * Created by 13 on 2019/5/24.
- */
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Constants {
-    public final static String FILE_UPLOAD_DIC = "/opt/deploy/upload/";//上传文件的默认url前缀，根据部署设置自行修改
+    public final static String FILE_UPLOAD_DIC = "E:/upload/";//上传文件的默认url前缀，根据部署设置自行修改
+  
 }
